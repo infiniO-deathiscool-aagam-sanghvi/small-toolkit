8018f196cb5f7927351dc73f3bfadcec38d19da6
infiniO-deathiscool-aagam-sanghvi <sanghvi.aagam1@gmail.com>
2026-03-31 20:35:59 +0530

    Added new features

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
+import random
 import time
 inputq = "start"
 while inputq.lower() != "stop":
-  inputq = input("Enter your command! : ")
-  inputa = inputq.split(" ")
-  if inputa[0] == "timer":
-    inputq = int(inputa[1])
-    while inputq != 0:
-      time.sleep(1)
-      print(inputq)
-      inputq -= 1
-    
-  
+  try:
+    inputq = input("Enter your command! : ")
+    inputa = inputq.split(" ")
+    if inputa[0] == "timer":
+      inputb = int(inputa[1])
+      while inputb != 0:
+        time.sleep(1)
+        print("\033[H\033[J", end="")
+        print(inputb)
+        inputb -= 1
+    elif inputa[0] == "coin":
+      print(random.choice(["Heads!","Tails!"]))
+    elif inputa[0] == "help":
+      print("Hi! I'm small-toolkit and I can do lots of things!\nWe can flip a coin (type \"coin\") or set a timer! (type \"timer 30\")")
+    elif inputa[0] == "random":
+      print(random.randint(int(inputa[1]),int(inputa[2])))
+  except:
+    print("Are you sure you typed enough parameters? Restart the program to continue using it.")
+      
