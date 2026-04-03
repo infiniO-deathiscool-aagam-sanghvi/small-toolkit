f661df29a23b621c0f7497186de130706edb5eb6
infiniO-deathiscool-aagam-sanghvi <sanghvi.aagam1@gmail.com>
2026-04-03 22:20:05 +0530

    New features! finally a cool project
    nah.

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import os
 import random
 import time
 inputq = "start"
-while inputq.lower() != "stop":
+q = 0
+definitions = []
+while inputq != "stop":
   try:
-    inputq = input("Enter your command! : ")
-    inputa = inputq.split(" ")
+    if q == 0:
+      inputq = input("Enter your command! : ")
+      inputa = inputq.split(" ")
     if inputa[0] == "timer":
       inputb = int(inputa[1])
       while inputb != 0:
@@ -19,7 +22,21 @@ while inputq.lower() != "stop":
       print("Hi! I'm small-toolkit and I can do lots of things!\nWe can flip a coin (type \"coin\") or set a timer! (type \"timer 30\")")
     elif inputa[0] == "random":
       print(random.randint(int(inputa[1]),int(inputa[2])))
-  inputq = "controlstr"
+    elif inputa[0] == "dice":
+      print(random.randint(1,6))
+    elif inputa[0] == "run":
+      del inputa[0]
+      print(eval(" ".join(inputa)))
+    elif inputa[0] == "def":
+      del inputa[0]
+      definitions.append([inputa.pop(1)," ".join(inputa)])
+    elif any(row[0] == inputa[0] for row in definitions):
+      inputa = inputa.slice(1)
+      q = 1
+      break
+    else:
+      print("ERROR.INVALID: Retry")
+    q = 0         
   except:
     print("Are you sure you typed enough parameters? Restart the program to continue using it.")
       
